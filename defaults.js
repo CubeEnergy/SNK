774d7f8ed94355ac783c4bcfe61165298c424abd
CubeEnergy <kevin.vorwerk@gmail.com>
2014-06-12 10:05:41 +0200

    Update defaults.js

--- defaults.js
+++ defaults.js
@@ -20,7 +20,7 @@ function fillFooter()
   var footer = document.getElementById("footer");
   footer.innerHTML = '<div class="wrapper"> \
       <a href="Impressum.html">Impressum</a> | <a href="mailto:thall1982@gmx.de">E-Mail: thal1982@gmx.de</a> | \
-      Desigend von Jonas Tödter und Kevin Vorwerk  \
+      Design von Jonas Tödter und Kevin Vorwerk  \
     </div>';
 }
 
